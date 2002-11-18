b097ae572657be1bc12dbe81eb5e15080a384e87
obecker <obecker>
2002-11-18 10:25:51 +0000

    automatic include of all jars in lib/

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-java -classpath classes:lib/log4j.jar net.sf.joost.Main "$@"
+# $Id: run.sh,v 1.2 2002/11/18 10:25:51 obecker Exp $
+# runs joost
+cp=$CLASSPATH
+# include all jars in the lib directory
+for i in lib/*.jar; do
+    cp=$cp:$i
+done
+java -classpath classes:$cp net.sf.joost.Main "$@"
+
