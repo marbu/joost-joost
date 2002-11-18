ce0e81d790c0492d538d42443cce3b13fc955e5b
obecker <obecker>
2002-11-18 16:14:27 +0000

    may run this script from other directories now

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-# $Id: run.sh,v 1.2 2002/11/18 10:25:51 obecker Exp $
+# $Id: run.sh,v 1.3 2002/11/18 16:14:27 obecker Exp $
 # runs joost
+
+basedir=`dirname $0`
 cp=$CLASSPATH
 # include all jars in the lib directory
-for i in lib/*.jar; do
+for i in $basedir/lib/*.jar; do
     cp=$cp:$i
 done
-java -classpath classes:$cp net.sf.joost.Main "$@"
+java -classpath $basedir/classes:$cp net.sf.joost.Main "$@"
 
