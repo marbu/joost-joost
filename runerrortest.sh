8b7078dbdf1e8de10cec4288989ac0f69fd7f057
obecker <obecker>
2002-12-23 08:42:29 +0000

    be more verbose - show stylesheet

--- runerrortest.sh
+++ runerrortest.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 for i in ../test/errors/*.stx; do
+  echo
   echo `basename ${i}`
-  run.sh build.xml ${i} > /dev/null
+  run.sh build.xml ${i} 2> xxx.tmp
+  cat -n $i
+  echo "----------------------------------------------------------------------"
+  cat xxx.tmp
+  echo "======================================================================"
 done
+rm xxx.tmp
