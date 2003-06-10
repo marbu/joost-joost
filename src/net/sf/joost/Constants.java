2ef14d795536d5e1e9d2068e5a147b9f32f01f0b
obecker <obecker>
2003-06-10 08:42:58 +0000

    added Joost extension functions namespace 'http://joost.sf.net/functions'

--- src/net/sf/joost/Constants.java
+++ src/net/sf/joost/Constants.java
@@ -1,5 +1,5 @@
 /*
- * $Id: Constants.java,v 2.1 2003/04/30 15:02:35 obecker Exp $
+ * $Id: Constants.java,v 2.2 2003/06/10 08:42:58 obecker Exp $
  * 
  * The contents of this file are subject to the Mozilla Public License 
  * Version 1.1 (the "License"); you may not use this file except in 
@@ -27,7 +27,7 @@ package net.sf.joost;
 
 /**
  * This interface contains constants shared between different classes. 
- * @version $Revision: 2.1 $ $Date: 2003/04/30 15:02:35 $
+ * @version $Revision: 2.2 $ $Date: 2003/06/10 08:42:58 $
  * @author Oliver Becker
  */
 public interface Constants
@@ -38,6 +38,10 @@ public interface Constants
    /** The STX functions namespace */
    public static final String FUNC_NS = 
       "http://stx.sourceforge.net/2003/functions";
+
+   /** The Joost extension functions namespace */
+   public static final String JOOST_FUNC_NS = 
+      "http://joost.sf.net/functions";
 
 
    /*
