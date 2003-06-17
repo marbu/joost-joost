0322e1d4cd8759c0c336df991d866c8c61c5ddbb
obecker <obecker>
2003-06-17 12:23:32 +0000

    compare with the contained error message

--- runerrortest.sh
+++ runerrortest.sh
@@ -1,5 +1,25 @@
 #!/bin/csh -f
-foreach i ( ../test/errors/error*.stx )
+# $Id: runerrortest.sh,v 1.4 2003/06/17 12:23:32 obecker Exp $
+# Runs a set of error cases and checks the expected error message
+# (This error message is always in line 3 of the input file)
+
+# Temporary file for the error message
+set tmp = err$$.tmp
+rm -f err[0-9]*.tmp
+
+if ( "$1" != "" ) then
+  set files = ../test/errors/error$1.stx
+  if ( ! -f ${files} ) then
+    echo File ${files} not found
+    exit
+  endif
+else
+  set files = ../test/errors/error*.stx
+endif
+
+foreach i ( ${files} )
   echo -n `basename ${i}`: 
-  run.sh build.xml ${i} |& sed -e 's/^[^:]*://; s/:-\?[0-9]*:/ -/' 
+  run.sh build.xml ${i} |& sed -e 's/^[^:]*://; s/:-\?[0-9]*:/:/' | tee ${tmp}
+  sed -e '1,2d; 4,$d' < ${i} | diff ${tmp} -
+  rm -f ${tmp}
 end
