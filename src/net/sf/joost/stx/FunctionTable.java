870a9441b4235c8a45ff6ebd11eda6dd470f78ac
obecker <obecker>
2003-10-20 16:56:26 +0000

    added escape-uri()

--- src/net/sf/joost/stx/FunctionTable.java
+++ src/net/sf/joost/stx/FunctionTable.java
@@ -1,5 +1,5 @@
 /*
- * $Id: FunctionTable.java,v 2.15 2003/09/10 14:13:14 obecker Exp $
+ * $Id: FunctionTable.java,v 2.16 2003/10/20 16:56:29 obecker Exp $
  * 
  * The contents of this file are subject to the Mozilla Public License 
  * Version 1.1 (the "License"); you may not use this file except in 
@@ -28,6 +28,10 @@ import org.xml.sax.Locator;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -46,7 +50,7 @@ import net.sf.joost.grammar.Tree;
 
 /**
  * Wrapper class for all STXPath function implementations.
- * @version $Revision: 2.15 $ $Date: 2003/09/10 14:13:14 $
+ * @version $Revision: 2.16 $ $Date: 2003/10/20 16:56:29 $
  * @author Oliver Becker
  */
 final public class FunctionTable implements Constants
@@ -94,6 +98,7 @@ final public class FunctionTable implements Constants
          new SubstringAfter(),
          new Translate(),
          new StringPad(),
+         new EscapeUri(),
          new Empty(),
          new Exists(),
          new ItemAt(),
@@ -1222,6 +1227,81 @@ final public class FunctionTable implements Constants
             buffer.append(str);
 
          return arg2.setString(buffer.toString()); // reuse Value object
+      }
+   }
+
+
+   /**
+    * The <code>escape-uri</code> function.
+    * Applies URI escaping rules.
+    * @see <a target="xq1xp2fo"
+    * href="http://www.w3.org/TR/xquery-operators/#func-escape-uri">
+    * fn:escape-uri in "XQuery 1.0 and XPath 2.0 Functions and Operators"</a>
+    */
+   final public class EscapeUri implements Instance 
+   {
+      /** @return 2 **/
+      public int getMinParCount() { return 2; }
+      /** @return 2 **/
+      public int getMaxParCount() { return 2; }
+      /** @return "escape-uri" */
+      public String getName() { return FNSP + "escape-uri"; }
+      
+      public Value evaluate(Context context, int top, Tree args)
+         throws SAXException, EvalException
+      {
+         Value v = args.left.evaluate(context, top);
+         String uri = v.convertToString().string;
+         boolean eReserved = args.right.evaluate(context, top)
+                                 .convertToBoolean().bool;
+
+         try {
+            char[] ch = uri.toCharArray();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream(4);
+            OutputStreamWriter osw = new OutputStreamWriter(baos, "UTF-8");
+            StringBuffer sb = new StringBuffer();
+            for (int i=0; i<ch.length; i++) {
+               // don't escape letters, digits, and marks
+               if ((ch[i] >= 'A' && ch[i] <= 'Z') ||
+                   (ch[i] >= 'a' && ch[i] <= 'z') ||
+                   (ch[i] >= '0' && ch[i] <= '9') ||
+                   (ch[i] >= '\'' && ch[i] <= '*') || // ' ( ) *
+                   "-_.!~".indexOf(ch[i]) != -1)
+                  sb.append(ch[i]);
+               // special rule for '%': check for two following hex digits
+               else if (ch[i] == '%' && i+2 < ch.length && 
+                        ((ch[i+1] >= 'A' && ch[i+1] <= 'F') ||
+                         (ch[i+1] >= 'a' && ch[i+1] <= 'f') ||
+                         (ch[i+1] >= '0' && ch[i+1] <= '9')) &&
+                        ((ch[i+2] >= 'A' && ch[i+2] <= 'F') ||
+                         (ch[i+2] >= 'a' && ch[i+2] <= 'f') ||
+                         (ch[i+2] >= '0' && ch[i+2] <= '9')))
+                  sb.append(ch[i]);
+               // don't escape reserved characters (if requested)
+               else if (!eReserved && ";/?:@&=+$,#[]".indexOf(ch[i]) != -1)
+                  sb.append(ch[i]);
+               // escape anything else
+               else {
+                  osw.write(ch[i]);
+                  osw.flush();
+                  byte ba[] = baos.toByteArray();
+                  for (int j=0; j<ba.length; j++) {
+                     int hex = (ba[j] >>> 4) & 0xF; // first 4 bits
+                     sb.append('%')
+                       .append(hex < 10 ? (char)((int)'0' + hex) 
+                                        : (char)((int)'A' + hex - 10));
+                     hex = ba[j] & 0xF; // last 4 bits
+                     sb.append(hex < 10 ? (char)((int)'0' + hex) 
+                                        : (char)((int)'A' + hex - 10));
+                  }
+                  baos.reset();
+               }
+            }
+            return v.setString(sb.toString());
+         }
+         catch (IOException ex) {
+            throw new EvalException("Fatal: " + ex.toString());
+         }
       }
    }
 
