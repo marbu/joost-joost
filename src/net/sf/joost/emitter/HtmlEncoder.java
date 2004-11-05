35c05ecf658b1b574403cba0138adaa89be74ff6
obecker <obecker>
2004-11-05 18:55:32 +0000

    add license for the original code

--- src/net/sf/joost/emitter/HtmlEncoder.java
+++ src/net/sf/joost/emitter/HtmlEncoder.java
@@ -1,3 +1,14 @@
+/*
+ * $Id: HtmlEncoder.java,v 1.2 2004/11/05 18:55:32 obecker Exp $
+ *
+ * The original code of this file has been taken from the Jodd project,
+ * see http://jodd.sourceforge.net/
+ *
+ * The contents of this file are subject to the BSD License, which is
+ * included at the end of this file.
+ *
+ * Modifications done by Thomas Behrends and Oliver Becker.
+ */
 package net.sf.joost.emitter;
 
 import java.io.UnsupportedEncodingException;
@@ -292,3 +303,40 @@ public final class HtmlEncoder {
 	}
 
 }
+
+/* ************ The BSD License coming with the original code: ************
+
+
+Copyright (c) 2003-2004, Jodd Team
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+Neither the name of the Jodd team nor the names of its contributors
+may be used to endorse or promote products derived from this
+software without specific prior written permission.
+
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
