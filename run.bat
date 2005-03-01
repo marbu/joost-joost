fb86ea131dc6c4a4af9c9b28863039fcdc57cdbb
obecker <obecker>
2005-03-01 18:19:37 +0000

    improved version (thanks to fikin)

--- run.bat
+++ run.bat
@@ -1,1 +1,16 @@
-java -cp classes;lib\commons-logging.jar;lib\log4j-1.2.8.jar net.sf.joost.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
+@echo off
+
+rem Set the following keys to 1 if you want to get this script working
+rem user settings HKEY_CURRENM_USER is also ok
+rem HKEY_LOCAL_MACHINE\Software\Microsoft\Command Processor\EnableExtensions
+rem HKEY_LOCAL_MACHINE\Software\Microsoft\Command Processor\DelayedExpansion
+rem or open a new shell with the following command:
+rem     cmd.exe /E:ON /V:ON
+
+set LIB_DIR=lib
+set CLASSPATH=classes
+rem alternative: CLASSPATH=joost.jar
+
+for %%f in (%LIB_DIR%\*.jar) do set CLASSPATH=!CLASSPATH!;%%f
+@echo on
+java -cp %CLASSPATH% net.sf.joost.Main %~1 %~2 %~3 %~4 %~5 %~6 %~7 %~8 %~9
