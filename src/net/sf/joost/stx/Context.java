e01ce30a076238107c1249878f3522894d5e8973
obecker <obecker>
2008-12-07 19:10:40 +0000

    removed copy() method

--- src/net/sf/joost/stx/Context.java
+++ src/net/sf/joost/stx/Context.java
@@ -1,22 +1,22 @@
 /*
- * $Id: Context.java,v 2.20 2007/07/15 15:20:41 obecker Exp $
- * 
- * The contents of this file are subject to the Mozilla Public License 
- * Version 1.1 (the "License"); you may not use this file except in 
+ * $Id: Context.java,v 2.21 2008/12/07 19:10:40 obecker Exp $
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
  *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the 
+ * for the specific language governing rights and limitations under the
  * License.
  *
  * The Original Code is: this file
  *
  * The Initial Developer of the Original Code is Oliver Becker.
  *
- * Portions created by  ______________________ 
- * are Copyright (C) ______ _______________________. 
+ * Portions created by  ______________________
+ * are Copyright (C) ______ _______________________.
  * All Rights Reserved.
  *
  * Contributor(s): Thomas Behrends.
@@ -24,17 +24,17 @@
 
 package net.sf.joost.stx;
 
-import java.util.Hashtable;
-import java.util.Stack;
-
-import javax.xml.transform.URIResolver;
-import javax.xml.transform.sax.TransformerHandler;
-
 import net.sf.joost.OutputURIResolver;
 import net.sf.joost.emitter.StxEmitter;
 import net.sf.joost.instruction.GroupBase;
 import net.sf.joost.instruction.NodeBase;
 import net.sf.joost.instruction.PSiblingsFactory;
+
+import java.util.Hashtable;
+import java.util.Stack;
+
+import javax.xml.transform.URIResolver;
+import javax.xml.transform.sax.TransformerHandler;
 
 import org.xml.sax.Locator;
 
@@ -42,7 +42,7 @@ import org.xml.sax.Locator;
 /**
  * Instances of this class provide context information while processing
  * an input document.
- * @version $Revision: 2.20 $ $Date: 2007/07/15 15:20:41 $
+ * @version $Revision: 2.21 $ $Date: 2008/12/07 19:10:40 $
  * @author Oliver Becker
  */
 public final class Context implements Cloneable
@@ -75,7 +75,7 @@ public final class Context implements Cloneable
    /** Encountered <code>stx:process-siblings</code> instruction */
    public PSiblingsFactory.Instance psiblings;
 
-   /** Hashtable for Stacks of group variables 
+   /** Hashtable for Stacks of group variables
        (key=group instance, value=Stack of Hashtables). */
    public Hashtable groupVars = new Hashtable();
 
@@ -84,7 +84,7 @@ public final class Context implements Cloneable
 
    /** External parameters passed to the transformation */
    public Hashtable globalParameters = new Hashtable();
-   
+
    /** Parameters passed to the next template */
    public Hashtable passedParameters = new Hashtable();
 
@@ -92,7 +92,7 @@ public final class Context implements Cloneable
    public ErrorHandlerImpl errorHandler = new ErrorHandlerImpl();
 
    /** The default TransformerHandlerResolver */
-   public TransformerHandlerResolverImpl defaultTransformerHandlerResolver = 
+   public TransformerHandlerResolverImpl defaultTransformerHandlerResolver =
       new TransformerHandlerResolverImpl();
 
    /** The target handler, set by <code>stx:process-<em>xxx</em></code>
@@ -101,49 +101,31 @@ public final class Context implements Cloneable
 
    /** The URIResolver for <code>stx:process-document</code> */
    public URIResolver uriResolver;
-   
+
    /** The OutputURIResolver for <code>stx:result-document</code> */
    public OutputURIResolver outputUriResolver;
 
-   /** 
+   /**
     * The message emitter for <code>stx:message</code>,
-    * either explicitely set by {@link Processor#setMessageEmitter} or 
+    * either explicitely set by {@link Processor#setMessageEmitter} or
     * automatically created in the first
-    * {@link net.sf.joost.instruction.MessageFactory.Instance#process} 
+    * {@link net.sf.joost.instruction.MessageFactory.Instance#process}
     * invocation.
     */
    public StxEmitter messageEmitter;
 
-   /**
-    * @return a copy of this object.
-    */
-   Context copy() // package private
-   {
-      Context context;
-      try {
-         context = (Context)clone();
-         context.localVars = new Hashtable();
-         context.errorHandler = new ErrorHandlerImpl();
-      }
-      catch (CloneNotSupportedException cnsex) {
-         throw new RuntimeException(cnsex.toString());
-      }
-
-      return context;
-   }
-   
    /** Instantiate a new emitter object for a new result event stream */
    public void pushEmitter(StxEmitter stxEmitter)
    {
       emitter = emitter.pushEmitter(stxEmitter);
    }
-   
+
    /** re-use a previous emitter for the event stream */
    public void pushEmitter(Emitter anEmitter)
    {
       anEmitter.prev = emitter;
       emitter = anEmitter;
-   }   
+   }
 
    /** Restore previous emitter after finishing a result event stream */
    public StxEmitter popEmitter()
